55d73c6a2ca9082936830d5cd06d916189cba9fe
Christopher Burton <christopherburton1020@gmail.com>
2022-12-21 01:38:57 -0700

    Reformat JS code

--- game.js
+++ game.js
@@ -1,69 +1,40 @@
 const GAME_OPTIONS = ["rock", "paper", "scissors"];
 
 function getComputerChoice() {
-  const computerSelection = GAME_OPTIONS[Math.floor(Math.random() * GAME_OPTIONS.length)];
-  return computerSelection;
+  const computerChoice = GAME_OPTIONS[Math.floor(Math.random() * GAME_OPTIONS.length)];
+  return computerChoice;
 }
 
-function getPlayerChoice() {
-  const playerSelection = prompt("Please select 'rock', 'paper', or 'scissors':").toLowerCase();
-  return playerSelection;
-}
+// function getPlayerChoice() {
+//   const playerChoice = prompt("Please select 'rock', 'paper', or 'scissors':").toLowerCase();
+//   return playerChoice;
+// }
 
-function checkWinner(computerSelection, playerSelection) {
-  if (computerSelection == playerSelection) {
+function checkWinner(playerSelection, computerSelection) {
+  if (playerSelection == computerSelection) {
     return "tie";
-  } else if ((playerSelection == "rock" && computerSelection == "scissors") || (playerSelection == "scissors" && computerSelection == "paper") || (playerSelection == "paper" && computerSelection == "rock")) {
+  } else if (
+    (playerSelection == "rock" && computerSelection == "scissors") ||
+    (playerSelection == "scissors" && computerSelection == "paper") ||
+    (playerSelection == "paper" && computerSelection == "rock")
+    ) {
     return "player";
   } else {
     return "computer";
   }
 }
 
-function declareWinner(winner) {
+function playRound(playerSelection, computerSelection) {
+  const winner = checkWinner(playerSelection, computerSelection);
   if (winner == "tie") {
     return "It's a tie.";
   } else if (winner == "player") {
-    return "You win!"
+    return `You win! ${playerSelection} beats ${computerSelection}!`;
   } else {
-    return "You lose!"
+    return `You lose! ${computerSelection} beats ${playerSelection}!`;
   }
 }
 
-
-// Broken. Sometimes randomly determines the winning move to be scissors, when paper and rock are chosen by the computer/player.
-function determineWinningMove(winner) {
-  if (winner == "player") {
-    return getPlayerChoice();
-  } else if (winner == "computer") {
-    return getComputerChoice();
-  } else {
-    return ""
-  }
-}
-
-function winMethod(winningMove) {
-  if (winningMove == "rock") {
-    return "Rock beats scissors!";
-  } else if (winningMove == "scissors") {
-    return "Scissors beats paper!";
-  } else if (winningMove == "paper") {
-    return "Paper beats rock!"
-  } else {
-    return ""
-  }
-}
-
-function playRound(computerSelection, playerSelection) {
-  const winner = checkWinner(computerSelection, playerSelection);
-  const winningMove = determineWinningMove(winner);
-  const winningMethod = winMethod(winningMove);
-  console.log(computerSelection);
-  console.log(playerSelection);
-  console.log(winner);
-  console.log(declareWinner(winner));
-  console.log(winningMove);
-  console.log(winningMethod);
-}
-// Return string that declares the winner
-playRound(getComputerChoice(), getPlayerChoice());
+const playerSelection = "scissors";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
